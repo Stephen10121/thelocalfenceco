aad94cf1740389e5f093c192b9e6c4b57a305bc5
Stephen Gruzin <stephengrzn@gmail.com>
2023-02-28 11:28:09 -0800

    Changed it to sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<div>
+  <header>
+    <h1>header</h1>
+  </header>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  div {
+    height: 100%;
+    width: 100%;
+    background-color: #dfdfdf;
+  }
+</style>
