a54a48f7ebca153c28439231f4261d44a096cc17
Stephen Gruzin <stephengrzn@gmail.com>
2023-04-28 19:10:36 -0700

    Updated the banner part

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,222 +1,197 @@
 <script lang="ts">
-  import { page } from "$app/stores";
-  import { onDestroy } from "svelte";
-  import Announcement from "../../components/Announcement.svelte";
-  import Footer from "../../components/Footer.svelte";
-  import Hamburger from "../../components/Hamburger.svelte";
-  import LargeNavLink from "../../components/LargeNavLink.svelte";
-  import SmallNavLink from "../../components/SmallNavLink.svelte";
-  import { exampleAnnouncementValues, scrollSomewhere, showAnnouncement } from "../../functions/store";
+	import { exampleAnnouncementValues, scrollSomewhere } from "../../functions/store";
+	import Announcement from "../../components/Announcement.svelte";
+	import LargeNavLink from "../../components/LargeNavLink.svelte";
+	import SmallNavLink from "../../components/SmallNavLink.svelte";
+	import Hamburger from "../../components/Hamburger.svelte";
+	import Footer from "../../components/Footer.svelte";
+	import { page } from "$app/stores";
+	import { onDestroy } from "svelte";
 
-  export let data;
+	export let data;
 
-  let scrollSomewhereValue: null | number = null;
-  const scrollSomewhereUnsubscribe = scrollSomewhere.subscribe((value) => scrollSomewhereValue=value);
+	let scrollSomewhereValue: null | number = null;
+	const scrollSomewhereUnsubscribe = scrollSomewhere.subscribe((value) => scrollSomewhereValue=value);
 
-  $: {
-    if (scrollSomewhereValue && scrollSomewhereValue!==null) {
-      main.scrollTo(0, scrollSomewhereValue);
-    }
-  }
+	$: if (scrollSomewhereValue && scrollSomewhereValue!==null) main.scrollTo(0, scrollSomewhereValue);
 
-  let showAnnouncement2 = false;
-  exampleAnnouncementValues.set(data.announcement);
-  const exampleAnnouncementValuesUnsubscribe = exampleAnnouncementValues.subscribe(value => showAnnouncement2 = value.show);
+	let showAnnouncement2 = false;
+	exampleAnnouncementValues.set(data.announcement);
+	const exampleAnnouncementValuesUnsubscribe = exampleAnnouncementValues.subscribe(value => showAnnouncement2 = value.show);
 
 
-  let showNav = false;
+	let showNav = false;
 
-  function linkClicked() {
-    showNav = false;
-  }
+	const linkClicked = () => showNav = false;
+	const pathMap = JSON.parse('{"/" : "-3px", "/about": "49px", "/contact": "108px", "/contact/quote": "108px", "/gallery": "170px"}');
 
-  function calculateTranslate(pathname: string) {
-    if (pathname === '/') return "-3px";
-    if (pathname === '/about') return "49px";
-    if (pathname === '/contact' || pathname === '/contact/quote') return "108px";
-    if (pathname === '/gallery') return "170px";
-    return '-3000px';
-  }
+	function calculateTranslate(pathname: string) {
+		if (!Object.keys(pathMap).includes(pathname)) return '-3000px';
+		return pathMap[pathname];
+	}
 
-  function calculatePathTitle(pathname: string) {
-    if (pathname === '/about') return "About";
-    if (pathname === '/contact') return "Contact";
-    if (pathname === '/gallery') return "Gallery";
-    return 'Local Fence Co';
-  }
+	function calculatePathTitle(pathname: string) {
+	if (pathname === '/about') return "About";
+	if (pathname === '/contact') return "Contact";
+	if (pathname === '/gallery') return "Gallery";
+	return 'Local Fence Co';
+	}
 
-  let main: HTMLElement;
-  // onMount(() => {
-  //   main.addEventListener("scroll", (event) => {
-  //     console.log(event);
-  //   });
-  // });
+	let main: HTMLElement;
 
-  function doSomething(stuff: any) {
-    console.log({stuff});
-  }
+	let paths: {route: string, name: string}[] = [
+	{route: "/", name: "Home"},
+	{route: "/about", name: "About"},
+	{route: "/contact", name: "Contact"},
+	{route: "/gallery", name: "Gallery"}
+	];
 
-  let paths: {route: string, name: string}[] = [
-    {route: "/", name: "Home"},
-    {route: "/about", name: "About"},
-    {route: "/contact", name: "Contact"},
-    {route: "/gallery", name: "Gallery"}
-  ];
-
-  onDestroy(() => {
-    scrollSomewhereUnsubscribe();
-    exampleAnnouncementValuesUnsubscribe();
-  });
+	onDestroy(() => {
+	scrollSomewhereUnsubscribe();
+	exampleAnnouncementValuesUnsubscribe();
+	});
 </script>
 
 {#if showAnnouncement2}
-  <Announcement strong={data.announcement.strong} value={data.announcement.val} on:close={() => (exampleAnnouncementValues.update((value) => {return{...value, show: false}}))} backgroundColor={data.announcement.backgroundColor} textColor={data.announcement.textColor} />
+<Announcement strong={data.announcement.strong} value={data.announcement.val} on:close={() => (exampleAnnouncementValues.update((value) => {return{...value, show: false}}))} backgroundColor={data.announcement.backgroundColor} textColor={data.announcement.textColor} />
 {/if}
 <header>
-  <section class="public">
-    <section class="fancy">
-      <a class="logo" href="/">
-        <h1>{calculatePathTitle($page.url.pathname)}</h1>
-      </a>
-      <section class="section-small">
-        <Hamburger active={showNav} on:click={() => (showNav = !showNav)} />
-      </section>
-      <nav class="section-large">
-        <ul>
-          <div class="selectedPointer" style="transform: translate({calculateTranslate($page.url.pathname)}, 50%)" />
-          {#each paths as onePath}
-            <LargeNavLink path={onePath.route}>{onePath.name}</LargeNavLink>
-          {/each}
-        </ul>
-      </nav>
-    </section>
-  </section>
-  <nav class="section-small-nav" style="transform: translateY({showNav ? '0' : '-100%'})">
-    <ul>
-      {#each paths as onePath}
-        <SmallNavLink path={onePath.route} pathname={$page.url.pathname} on:click={linkClicked}>{onePath.name}</SmallNavLink>
-      {/each}
-    </ul>
-  </nav>
+<section class="public">
+<section class="fancy">
+<a class="logo" href="/">
+<h1>{calculatePathTitle($page.url.pathname)}</h1>
+</a>
+<section class="section-small">
+<Hamburger active={showNav} on:click={() => (showNav = !showNav)} />
+</section>
+<nav class="section-large">
+<ul>
+<div class="selectedPointer" style="transform: translate({calculateTranslate($page.url.pathname)}, 50%)" />
+{#each paths as onePath}
+<LargeNavLink path={onePath.route}>{onePath.name}</LargeNavLink>
+{/each}
+</ul>
+</nav>
+</section>
+</section>
+<nav class="section-small-nav" style="transform: translateY({showNav ? '0' : '-100%'})">
+<ul>
+{#each paths as onePath}
+<SmallNavLink path={onePath.route} pathname={$page.url.pathname} on:click={linkClicked}>{onePath.name}</SmallNavLink>
+{/each}
+</ul>
+</nav>
 </header>
 <section bind:this={main} class="main-section" style="--minus-height: {showAnnouncement2 ? "100px" : "70px"}">
-  <slot />
-  <Footer />
+<slot />
+<Footer />
 </section>
-  
+
 <style>
-  header {
-    height: 70px;
-    width: 100%;
-    position: relative;
-  }
+header {
+height: 70px;
+width: 100%;
+position: relative;
+}
 
-  .main-section {
-    --minus-height: 100px;
-    overflow: auto;
-    width: 100%;
-    height: calc(100% - var(--minus-height));
-    scroll-behavior: smooth;
-    /* display: grid;
-    grid-template-rows: auto 200px; */
-  }
+.main-section {
+--minus-height: 100px;
+overflow: auto;
+width: 100%;
+height: calc(100% - var(--minus-height));
+scroll-behavior: smooth;
+}
 
-  /* .main-section main {
-    height: 100%;
-  } */
+.public {
+height: 70px;
+background-color: #ffffff;
+width: 100%;
+position: relative;
+z-index: 200;
+}
 
-  .public {
-    height: 70px;
-    background-color: #ffffff;
-    width: 100%;
-    position: relative;
-    z-index: 200;
-    /* box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px; */
-  }
+.fancy {
+position: absolute;
+height: 70px;
+background-color: #dfdfdf;
+width: 100%;
+left: 0;
+top: 0;
+border-radius: 0;
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 0 20px;
+box-shadow: none;
+}
 
-  .fancy {
-    position: absolute;
-    height: 70px;
-    background-color: #dfdfdf;
-    width: 100%;
-    left: 0;
-    top: 0;
-    border-radius: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0 20px;
-    box-shadow: none;
-  }
+.logo {
+text-decoration: none;
+}
 
-  .logo {
-    text-decoration: none;
-  }
+.logo h1 {
+font-family: "Poppins", sans-serif;
+font-size: 1.5rem;
+color: #28583b;
+}
 
-  .logo h1 {
-    font-family: "Poppins", sans-serif;
-    font-size: 1.5rem;
-    color: #28583b;
-  }
+.section-small-nav {
+width: 100%;
+background-color: #dfdfdf;
+position: absolute;
+top: 100%;
+left: 0;
+z-index: 180;
+transition: transform 0.25s linear;
+}
 
-  .section-small-nav {
-    width: 100%;
-    background-color: #dfdfdf;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    z-index: 180;
-    transition: transform 0.25s linear;
-  }
+.section-small-nav ul {
+display: flex;
+padding: 5px;
+gap: 5px;
+flex-direction: column;
+}
 
-  .section-small-nav ul {
-    display: flex;
-    padding: 5px;
-    gap: 5px;
-    flex-direction: column;
-  }
+.section-large {
+display: none;
+}
 
-  .section-large {
-    display: none;
-  }
+.section-large ul {
+display: flex;
+gap: 10px;
+position: relative;
+}
 
-  .section-large ul {
-    display: flex;
-    gap: 10px;
-    position: relative;
-  }
+.selectedPointer {
+width: 50px;
+height: 4px;
+position: absolute;
+background-color: #28583b;
+bottom: -50%;
+left: 0;
+transform: translate(0, 50%);
+transition: transform 0.1s linear;
+}
 
-  .selectedPointer {
-    width: 50px;
-    height: 4px;
-    position: absolute;
-    background-color: #28583b;
-    bottom: -50%;
-    left: 0;
-    transform: translate(0, 50%);
-    transition: transform 0.1s linear;
-  }
+@media (min-width: 600px) {
+.fancy {
+width: 90%;
+left: 5%;
+top:15px;
+border-radius: 10px;
+box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+}
 
-  @media (min-width: 600px) {
-    .fancy {
-      width: 90%;
-      left: 5%;
-      top:15px;
-      border-radius: 10px;
-      box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    }
+.section-small-nav {
+transform: translateY(-100%) !important;
+}
 
-    .section-small-nav {
-      transform: translateY(-100%) !important;
-    }
+.section-large {
+display: block;
+}
 
-    .section-large {
-      display: block;
-    }
-
-    .section-small {
-      display: none;
-    }
-  }
+.section-small {
+display: none;
+}
+}
 </style>
-  
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,47 +1,23 @@
 <script lang="ts">
-  //@ts-ignore
-  import Image from "svelte-image";
-  import PopupBox from "../../components/PopupBox.svelte";
+  import SmallDesc from "../../components/mainPage/SmallDesc.svelte";
   import Section1 from "../../components/mainPage/Section1.svelte";
-  import Section2 from "../../components/mainPage/Section2.svelte";
-    import { onMount } from "svelte";
-    import PictureV2 from "../../components/PictureV2.svelte";
-    import FadeImages from "../../components/FadeImages.svelte";
-    import SmallDesc from "../../components/mainPage/SmallDesc.svelte";
-  let showPopup = false;
+  import FadeImages from "../../components/FadeImages.svelte";
 
   let main: HTMLElement;
-
-  onMount(() => {
-    console.log(main.getBoundingClientRect());
-  });
 </script>
 
 <div class="image">
-  <!-- <PictureV2 alt="Fence 1" url="fence4" /> -->
   <FadeImages />
 </div>
 <main bind:this={main}>
   <Section1 />
-  <!-- <Section2 /> -->
   <SmallDesc />
-  {#if showPopup}
-    <PopupBox on:close={() => showPopup=false}>
-      <section class="disclaimer">
-        <h1>Dev Mode.</h1>
-        <p>*** All the images/text you see here are just placeholders and are only used for development.***</p>
-      </section>
-    </PopupBox>
-  {/if}
 </main>
 
 <style>
   main {
     width: 100%;
     min-height: 200%;
-    /* display: grid;
-    grid-template-rows: 100% auto; */
-    /* background-color: red;*/
   }
   
   .image {
@@ -51,38 +27,5 @@
     z-index: -1;
     width: 100vw;
     height: 100vh;
-    /* display: flex;
-    align-items: center;
-    justify-content: center; */
-    /* min-height: 100%; */
-    /* object-fit: cover; */
-    /* background: linear-gradient(#ffffff, #000000); */
-    /* background-image: url("/fenc1.jpg"); */
   }
-
-  .disclaimer {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    gap: 20px;
-    padding: 0 20px;
-  }
-
-  .disclaimer h1 {
-    color: red;
-    font-size: 2rem;
-    font-family: "Poppins",sans-serif;
-  }
-  .disclaimer p {
-    font-family: "Roboto", sans-serif;
-  }
-
-  /* @media (min-width: 1000px) {
-    .box {
-        width: 40%;
-    }
-  } */
 </style>
